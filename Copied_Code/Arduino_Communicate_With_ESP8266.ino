d4b938321b3bb0d6d6ef6fb8cbf15281683b3ebd
Devon Riter <63297761+devonriter@users.noreply.github.com>
2020-07-04 12:01:08 -0500

    Update Arduino_Communicate_With_ESP8266.ino

--- Copied_Code/Arduino_Communicate_With_ESP8266.ino
+++ Copied_Code/Arduino_Communicate_With_ESP8266.ino
@@ -24,32 +24,15 @@
   Please see attached LICENSE.txt file for details.
 ------------------------------------------------------------------------------*/
 #include <ArduinoJson.h>
-#include <TinyGPS++.h>
-#include <SoftwareSerial.h>
-
-static const int RXPin = 4, TXPin = 3;
-static const uint32_t GPSBaud = 9600;
-
-// The TinyGPS++ object
-TinyGPSPlus gps;
-
-// The serial connection to the GPS device
-SoftwareSerial ss(RXPin, TXPin);
 
 String message = "";
 bool messageReady = false;
 
 void setup() {
   Serial.begin(9600);
-  ss.begin(GPSBaud);
 }
 
 void loop() {
-  // printFloat(gps.location.lat(), gps.location.isValid(), 11, 6);
-//  float y = float(gps.location.lng(), gps.location.isValid(), 12, 6);
-//  Serial.print(x);
-  // delay(5000);
-  
   // Monitor serial communication
   while(Serial.available()) {
     message = Serial.readString();
@@ -70,38 +53,10 @@ void loop() {
     if(doc["type"] == "request") {
       doc["type"] = "response";
       // Get data from analog sensors
-      doc["latitude"] = float(gps.location.lat(), gps.location.isValid(), 11, 6);
-      doc["longitude"] = float(gps.location.lng(), gps.location.isValid(), 12, 6);
+      doc["distance"] = analogRead(A0);
+      doc["gas"] = analogRead(A1);
       serializeJson(doc,Serial);
     }
     messageReady = false;
   }
-}
-static void printFloat(float val, bool valid, int len, int prec)
-{
-  if (!valid)
-  {
-    while (len-- > 1)
-      Serial.print('*');
-    Serial.print(' ');
-  }
-  else
-  {
-    Serial.print(val, prec);
-    int vi = abs((int)val);
-    int flen = prec + (val < 0.0 ? 2 : 1); // . and -
-    flen += vi >= 1000 ? 4 : vi >= 100 ? 3 : vi >= 10 ? 2 : 1;
-    for (int i=flen; i<len; ++i)
-      Serial.print(' ');
-  }
-  smartDelay(0);
-}
-static void smartDelay(unsigned long ms)
-{
-  unsigned long start = millis();
-  do 
-  {
-    while (ss.available())
-      gps.encode(ss.read());
-  } while (millis() - start < ms);
 }
