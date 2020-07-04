4e2feb5d8dfb6919c4ced1e7d97c39d4540b420d
Devon Riter <63297761+devonriter@users.noreply.github.com>
2020-07-04 11:57:11 -0500

    Update and rename Prototype_Code.ino to Prototype_Arduino_Code.ino

---
+++ Prototype_Arduino_Code.ino
@@ -0,0 +1,64 @@
+#include <ArduinoJson.h>
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+
+static const int RXPin = 4, TXPin = 3;
+static const uint32_t GPSBaud = 9600;
+
+// The TinyGPS++ object
+TinyGPSPlus gps;
+
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
+
+String message = "";
+bool messageReady = false;
+
+void setup() {
+  Serial.begin(9600);
+  ss.begin(GPSBaud);
+}
+
+void loop() {
+  
+ smartDelay(1000);
+ response ();
+ 
+}
+
+void response {
+  doc["type"] = "response";
+  // Get data from sensors
+  doc["latitude"] = 43.98756;
+  doc["longitude"] = 74.90382;
+  serializeJson(doc,Serial);
+}
+
+static void printFloat(float val, bool valid, int len, int prec)
+{
+  if (!valid)
+  {
+    while (len-- > 1)
+      Serial.print('*');
+    Serial.print(' ');
+  }
+  else
+  {
+    Serial.print(val, prec);
+    int vi = abs((int)val);
+    int flen = prec + (val < 0.0 ? 2 : 1); // . and -
+    flen += vi >= 1000 ? 4 : vi >= 100 ? 3 : vi >= 10 ? 2 : 1;
+    for (int i=flen; i<len; ++i)
+      Serial.print(' ');
+  }
+  smartDelay(0);
+}
+static void smartDelay(unsigned long ms)
+{
+  unsigned long start = millis();
+  do 
+  {
+    while (ss.available())
+      gps.encode(ss.read());
+  } while (millis() - start < ms);
+}
