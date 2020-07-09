e7487e4443a3778d76fade2145a2e751535f83ab
Devon Riter <63297761+devonriter@users.noreply.github.com>
2020-07-09 12:49:35 -0500

    Update Prototype_Arduino_Code.ino

--- Prototype_Arduino_Code.ino
+++ Prototype_Arduino_Code.ino
@@ -1,55 +1,71 @@
 /*------------------------------------------------------------------------------
-This code get's loaded on to the Arduino and sends GPS location to an ESP8266 when a distress button is pressed.
+This code get's loaded on to the Arduino and should send your GPS location to an ESP8266 when a distress button is pressed.
 If you don't have a GPS module connected, it will just send 0.00000 as the latitude and longitude.
 ------------------------------------------------------------------------------*/
-
-//Libraries needed
 #include <ArduinoJson.h>
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 #include <Wire.h>
 
-static const int RXPin = 4, TXPin = 3;
-static const uint32_t GPSBaud = 9600;
-TinyGPSPlus gps; // The TinyGPS++ object
-SoftwareSerial ss(RXPin, TXPin); // The serial connection to the GPS device
-String gpslat;
-String gpslong;
+SoftwareSerial gpsserial(4, 3); // The serial connection to the GPS device RXPin = 4, TXPin = 3;
 
 int DistressPin= 11;  // Connect the distress button data pin to digital pin 11 on the arduino
-
 int DistressState = 0;
+String message = "";
+bool messageReady = false;
+String latitude = "Safe";
+String longitude = "Safe";
 
 void setup() {
   Serial.begin(9600);
-  ss.begin(GPSBaud);
+  gpsserial.begin(9600);
   pinMode(DistressPin, INPUT);
 }
 
-void loop() 
-{
+void loop() {
   int DistressState = digitalRead(DistressPin);
-  if (DistressState == 1) //If button gets pressed we send lat and long to ESP8266
+  if (DistressState == 1) //If button gets pressed we save latitude and longitude
     {  
-      //Here GPS Data gets sent to the ESP8266 via a JSON response
-      smartDelay(1000);
-      gpslat = String(gps.location.lat(), 6);
-      gpslong = String(gps.location.lng(), 6);
-      DynamicJsonDocument doc(1024);
-      doc["type"] = "response";
-      doc["latitude"] = gpslat;
-      doc["longitude"] = gpslong;
-      serializeJson(doc,Serial);
-      smartDelay(1000);
+      TinyGPSPlus gps; // The TinyGPS++ object
+      delay(1000);
+      latitude = String(gps.location.lat(), 6);
+      longitude = String(gps.location.lng(), 6);
     }
-}
-static void smartDelay(unsigned long ms) 
-  {
-  unsigned long start = millis();
-  do 
-    {
-      while (ss.available())
-        gps.encode(ss.read());
-    } 
-  while (millis() - start < ms);
+ 
+  while(Serial.available()) {
+    message = Serial.readString();
+    messageReady = true;
   }
+  // Only process message if there's one
+  if(messageReady) {
+    // The only messages we'll parse will be formatted in JSON
+    DynamicJsonDocument doc(1024); // ArduinoJson version 6+
+    // Attempt to deserialize the message
+    DeserializationError error = deserializeJson(doc,message);
+    if(error) {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.c_str());
+      messageReady = false;
+      return;
+    }
+    if(doc["type"] == "request") {
+      doc["type"] = "response";
+      // Get data from analog sensors
+      doc["latitude"] = latitude;
+      doc["longitude"] = longitude;
+      serializeJson(doc,Serial);
+    }
+    messageReady = false;
+  }
+}
+
+//static void smartDelay(unsigned long ms) 
+//  {
+//  unsigned long start = millis();
+//  do 
+//    {
+//      while (gpsserial.available())
+//        gps.encode(gpsserial.read());
+//    } 
+//  while (millis() - start < ms);
+//  }
