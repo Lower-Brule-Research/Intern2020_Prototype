e8065297050d369221b09efdca724796b8ea984c
Devon Riter <63297761+devonriter@users.noreply.github.com>
2020-07-22 11:20:12 -0500

    Update Prototype_Arduino_Code.ino

--- Prototype_Arduino_Code.ino
+++ Prototype_Arduino_Code.ino
@@ -1,71 +1,85 @@
 /*------------------------------------------------------------------------------
-This code get's loaded on to the Arduino and should send your GPS location to an ESP8266 when a distress button is pressed.
-If you don't have a GPS module connected, it will just send 0.00000 as the latitude and longitude.
+This code get's loaded on to the Arduino and should date, time, and heartrate on an LCD display.
 ------------------------------------------------------------------------------*/
-#include <ArduinoJson.h>
-#include <TinyGPS++.h>
-#include <SoftwareSerial.h>
 #include <Wire.h>
+#include "MAX30105.h"
+#include "heartRate.h"          //Heart rate calculating algorithm
+#include <Wire.h>
+#include <virtuabotixRTC.h> //Libraries needed
+#include <LiquidCrystal_I2C.h>
 
-SoftwareSerial gpsserial(4, 3); // The serial connection to the GPS device RXPin = 4, TXPin = 3;
+MAX30105 particleSensor; // initialize MAX30102 with I2C
+const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
+byte rates[RATE_SIZE]; //Array of heart rates
+byte rateSpot = 0;
+long lastBeat = 0; //Time at which the last beat occurred
+float beatsPerMinute;
+int beatAvg;
 
-int DistressPin= 11;  // Connect the distress button data pin to digital pin 11 on the arduino
-int DistressState = 0;
-String message = "";
-bool messageReady = false;
-String latitude = "Safe";
-String longitude = "Safe";
+LiquidCrystal_I2C lcd(0x27,16,2); //Wiring CLK -> 6 , DAT -> 7, Reset -> 8
+virtuabotixRTC myRTC(6, 7, 8); //If you change the wiring change the pins here also
 
 void setup() {
-  Serial.begin(9600);
-  gpsserial.begin(9600);
-  pinMode(DistressPin, INPUT);
+ Serial.begin(9600);
+  lcd.init();                      // initialize the lcd
+  lcd.backlight();
+  myRTC.setDS1302Time(15, 31, 15, 5, 12, 1, 2018); //Here you write your actual time/date 
+  //but remember to "comment/remove" this function once you're done as I did
+  //The setup is done only one time and the module will continue counting it automatically
+  
+  // Initialize sensor
+  byte ledBrightness = 70; //Options: 0=Off to 255=50mA
+  byte sampleAverage = 1; //Options: 1, 2, 4, 8, 16, 32
+  byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  int sampleRate = 400; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 69; //Options: 69, 118, 215, 411
+  int adcRange = 16384; //Options: 2048, 4096, 8192, 16384
+
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+  
 }
 
 void loop() {
-  int DistressState = digitalRead(DistressPin);
-  if (DistressState == 1) //If button gets pressed we save latitude and longitude
-    {  
-      TinyGPSPlus gps; // The TinyGPS++ object
-      delay(1000);
-      latitude = String(gps.location.lat(), 6);
-      longitude = String(gps.location.lng(), 6);
-    }
- 
-  while(Serial.available()) {
-    message = Serial.readString();
-    messageReady = true;
-  }
-  // Only process message if there's one
-  if(messageReady) {
-    // The only messages we'll parse will be formatted in JSON
-    DynamicJsonDocument doc(1024); // ArduinoJson version 6+
-    // Attempt to deserialize the message
-    DeserializationError error = deserializeJson(doc,message);
-    if(error) {
-      Serial.print(F("deserializeJson() failed: "));
-      Serial.println(error.c_str());
-      messageReady = false;
-      return;
-    }
-    if(doc["type"] == "request") {
-      doc["type"] = "response";
-      // Get data from analog sensors
-      doc["latitude"] = latitude;
-      doc["longitude"] = longitude;
-      serializeJson(doc,Serial);
-    }
-    messageReady = false;
-  }
-}
+ lcd.clear(); //Here after clearing the LCD we take the time from the module and print it on the screen with usual LCD functions
+ myRTC.updateTime();
+ lcd.setCursor(0,0);
+ lcd.print(myRTC.dayofmonth);
+ lcd.print("/");
+ lcd.print(myRTC.month);
+ lcd.print("/");
+ lcd.print(myRTC.year);
+ lcd.setCursor(0,1);
+ lcd.print(myRTC.hours);
+ lcd.print(":");
+ lcd.print(myRTC.minutes);
+ lcd.print(":");
+ lcd.print(myRTC.seconds);
+  
+ particleSensor.check(); //Check the sensor
+  while (particleSensor.available()) {
+    particleSensor.nextSample();  
+    long irValue = particleSensor.getIR();    //Reading the IR value it will permit us to know if there's a finger on the sensor or not
+    if (checkForBeat(irValue) == true)                        //If a heart beat is detected
+    {
+      long delta = millis() - lastBeat;                   //Measure duration between two beats
+      lastBeat = millis();
+      beatsPerMinute = 60 / (delta / 1000.0);           //Calculating the BPM
+      if (beatsPerMinute < 255 && beatsPerMinute > 20)               //To calculate the average we strore some values (4) then do some math to calculate the average
+      {
+        rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
+        rateSpot %= RATE_SIZE; //Wrap variable
 
-//static void smartDelay(unsigned long ms) 
-//  {
-//  unsigned long start = millis();
-//  do 
-//    {
-//      while (gpsserial.available())
-//        gps.encode(gpsserial.read());
-//    } 
-//  while (millis() - start < ms);
-//  }
+        //Take average of readings
+        beatAvg = 0;
+        for (byte x = 0 ; x < RATE_SIZE ; x++)
+          beatAvg += rates[x];
+        beatAvg /= RATE_SIZE;
+        lcd.setCursor(0,1);
+        lcd.print("BPM=");
+        lcd.print(beatAvg);
+        }
+      }
+    }
+  
+ delay(1000);
+}
